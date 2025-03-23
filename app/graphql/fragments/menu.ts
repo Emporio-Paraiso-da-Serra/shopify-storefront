96a2f7b16350fda4e6f16db5a03f8e95cd3833eb
Juliano Sill <sill.juliano@gmail.com>
2025-03-23 01:08:37 -0300

    chore(graphql): create `MENU_FRAGMENT` and `header.query.ts`

---
+++ app/graphql/fragments/menu.ts
@@ -0,0 +1,22 @@
+export const MENU_FRAGMENT = `#graphql
+  fragment MenuItem on MenuItem {
+    id
+    title
+    url
+  }
+  fragment ChildMenuItem on MenuItem {
+    ...MenuItem
+  }
+  fragment ParentMenuItem on MenuItem {
+    ...MenuItem
+    items {
+      ...ChildMenuItem
+    }
+  }
+  fragment Menu on Menu {
+    id
+    items {
+      ...ParentMenuItem
+    }
+  }
+` as const
